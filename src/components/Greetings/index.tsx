1ae82c31f5ca188417244c0e46e735eaa0956b2e
Evandro Carvalho Ferreira <dev.evandro@gmail.com>
2022-11-26 16:10:01 -0300

    Initial commit

---
+++ src/components/Greetings/index.tsx
@@ -0,0 +1,13 @@
+/* import { Button } from '../Button' */
+
+export function Greetings() {
+  /* function handleSayHello() {
+    window.Main.sendMessage('Hello World')
+
+    console.log('Message sent! Check main process log in terminal.')
+  } */
+
+  return (
+    <p style={{ color: 'white' }}>template</p>
+  )
+}
